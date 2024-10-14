f08e3bd2072ccbfbe6cd3f080798be0f666b3458
Vadim Karcev <cfg888powerfull@gmail.com>
2024-10-15 00:26:14 +0300

    Added checking for errors in multiple files

--- main.cxx
+++ main.cxx
@@ -8,12 +8,15 @@ int main(int argc, char** argv)
     if (argc == 1)
         dprintf("Filename isn't provided!\nUsage: %s [filename, ..]\n", argv[0]);
 
+    int err_count = 0;
+
     for (int f_no = 1; f_no < argc; f_no++)
     {
         auto solver = DPLLSolver();
         if (solver.loadDIMACS(argv[f_no]) != DPLLSolver::ERROR::OK)
         {
             dprintf("Cannot load CNF from file %s\n", argv[f_no]);
+            err_count++;
             continue;
         }
         
@@ -32,5 +35,5 @@ int main(int argc, char** argv)
         }
     }
 
-    return 0;
+    return err_count != 0;
 }
