abade86261ef89346ea7754ad3384ae4a3e93316
Vadim Karcev <cfg888powerfull@gmail.com>
2024-10-15 01:36:42 +0300

    dprintf indentation fixed

--- dprintf.hxx
+++ dprintf.hxx
@@ -5,7 +5,7 @@
 
 #ifdef DEBUG
 
-	#define dprintf(format_string, ...)                                     \
+    #define dprintf(format_string, ...)                                     \
         {                                                                   \
             printf("\x1b[35m>> debug from <%s::%d>: ", __FILE__, __LINE__); \
             printf(format_string, __VA_ARGS__);                             \
@@ -16,7 +16,7 @@
 						
 #else
 
-	#define dprintf(format_string, ...)
+    #define dprintf(format_string, ...)
     #define debug if(false)
 	
 #endif
