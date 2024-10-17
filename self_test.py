1c2770a03ec169ab2295232dc115e90338660e82
Vadim Karcev <cfg888powerfull@gmail.com>
2024-10-18 00:45:19 +0300

    Self testing script now can check if test passed or failed based on test name

--- self_test.py
+++ self_test.py
@@ -28,10 +28,12 @@ for test in tests:
     results[f'{folder}/{test}'] = {'success': success, 'output': output}
     
 duration = (mcs() - start) / 1000
-print(f'Passed {len(tests)} tests in {duration:.0f} ms')
+print(f'Finished {len(tests)} tests in {duration:.0f} ms')
 
-succ_count = 0
-err_count = 0
+exited_normally = 0
+exited_abnormally = 0
+passed = 0
+failed = 0
 SAT_tests = []
 UNSAT_tests = []
 unclear_tests = []
@@ -48,17 +50,26 @@ for test in results:
     results[test]['output'] = newlines
 
     if results[test]['success']: 
-        succ_count += 1
+        exited_normally += 1
 
         if results[test]['output'][-1].endswith('UNSAT'):
             UNSAT_tests.append(test)
+            if test.split('/')[-1].lower().startswith('unsat') or test.lower().startswith('uuf'): passed += 1
+            else: failed += 1
+
         elif results[test]['output'][-1].endswith('SAT'):
             SAT_tests.append(test)
+            if test.split('/')[-1].lower().startswith('sat') or test.lower().startswith('uf'): passed += 1
+            else: failed += 1
+
         else:
             unclear_tests.append(test)
-    else: err_count += 1
+            failed += 1
 
-print(f'Completed correctly: {succ_count}/{succ_count + err_count}')
+    else: exited_abnormally += 1
+
+print(f'Exited normally: {exited_normally}/{exited_normally + exited_abnormally}')
+print(f'Passed: {passed}/{passed + failed}')
 print(f'SAT in {len(SAT_tests)} tests')
 print(f'UNSAT in {len(UNSAT_tests)} tests')
 print(f'Unclear output in {len(unclear_tests)} tests')
@@ -67,10 +78,12 @@ with open(f'{folder}_results.json', 'w') as res_file:
     res_file.write(
         dumps({
             'duration': f'{duration:.0f} ms',
-            'completed': f'{succ_count}/{succ_count + err_count}',
-            'terminated': f'{err_count}/{succ_count + err_count}',
-            'SAT': f'{len(SAT_tests)}/{succ_count}',
-            'UNSAT': f'{len(UNSAT_tests)}/{succ_count}',
-            'confusing_output': f'{len(unclear_tests)}/{succ_count}',
+            'passed': f'{passed}/{passed + failed}',
+            'failed': f'{failed}/{passed + failed}',
+            'exited_normally': f'{exited_normally}/{exited_normally + exited_abnormally}',
+            'exited_abnormally': f'{exited_abnormally}/{exited_normally + exited_abnormally}',
+            'SAT': f'{len(SAT_tests)}/{exited_normally}',
+            'UNSAT': f'{len(UNSAT_tests)}/{exited_normally}',
+            'confusing_output': f'{len(unclear_tests)}/{exited_normally}',
             'results': results
         }, ensure_ascii=False, indent=2))
