cf26bbca908bb21fd89708eabb6db48772e18c2a
Vadim Karcev <cfg888powerfull@gmail.com>
2024-10-18 17:41:35 +0300

    Fixed file:line format to support auto-navigation

--- dprintf.hxx
+++ dprintf.hxx
@@ -7,7 +7,7 @@
 
     #define dprintf(format_string, ...)                                     \
         {                                                                   \
-            printf("\x1b[35m>> debug from <%s::%d>: ", __FILE__, __LINE__); \
+            printf("\x1b[35m>> debug from %s:%d: ", __FILE__, __LINE__); \
             printf(format_string, __VA_ARGS__);                             \
             printf("\x1b[0m");                                              \
         }
